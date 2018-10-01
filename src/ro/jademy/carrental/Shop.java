32369216734f70adb0f806fcd535f7115e3220b1
Nicu Seder <nicu.seder@gmail.com>
2018-10-01 16:25:16 +0300

    Filter methods added

--- src/ro/jademy/carrental/Shop.java
+++ src/ro/jademy/carrental/Shop.java
@@ -19,14 +19,16 @@ public class Shop {
     private List<Salesman> shopEmployees = new ArrayList<>();
     private List<Car> cars = new ArrayList();
     private Scanner scanner = new Scanner(System.in);
-    // Q: what fields and methods should this class contain?
+    private BigDecimal getIncome = new BigDecimal(0);
 
+    //constructor where employees and cars are initialized
     public Shop(){
         initEmployees();
         initCars();
         loginPage();
     }
 
+    //login method
     private boolean login() {
         // TODO: implement a basic user login
         String username = askUserName();
@@ -40,6 +42,7 @@ public class Shop {
         return false;
     }
 
+    //start the app
     public void start(){
         if(login()){
             showMenu();
@@ -48,6 +51,7 @@ public class Shop {
         }
 
     }
+    //If credentials invalid then show options as try again or exit
     private void menuInvalidCredentials(){
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------");
         System.out.println("Invalid Credentials");
@@ -68,7 +72,7 @@ public class Shop {
 
     }
 
-
+    //Menu design
     private void showMenu() {
 
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------");
@@ -80,8 +84,9 @@ public class Shop {
         System.out.println("2. List available cars");
         System.out.println("3. List rented cars");
         System.out.println("4. Check income");
-        System.out.println("5. Logout");
-        System.out.println("6. Exit");
+        System.out.println("5. Filter");
+        System.out.println("6. Logout");
+        System.out.println("7. Exit");
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------");
         checkChosenMenuOption();
     }
@@ -92,20 +97,39 @@ public class Shop {
             case 1:listAllCars();break;
             case 2:listAvailableCars();break;
             case 3:listUnavailableCars();break;
+            case 4:checkIncome();break;
+            case 5:showListMenuOptions();
+            case 6:start();break;
+            case 7:System.exit(0);
             default:break;
         }
     }
 
-    public void showListMenuOptions() {
+    private void checkFilterOption(){
+        switch (scanner.nextInt()){
+            case 1:filterByMake();break;
+            case 2:filterByModel();break;
+            case 3:filterByBudget();break;
+            case 4:checkIncome();break;
+            case 5:showListMenuOptions();
+            default:break;
+        }
+    }
 
+    //Filter options design
+    private void showListMenuOptions() {
+        System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------");
         System.out.println("Select an action from below:");
+        System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------");
         System.out.println("1. Filter by make");
         System.out.println("2. Filter by model");
         System.out.println("3. Filter by budget");
+
         // TODO: add additional filter methods based on car specs
 
         System.out.println("4. Back to previous menu");
-
+        System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------");
+        checkFilterOption();
     }
 
     public void calculatePrice(int numberOfDays) {
@@ -118,6 +142,7 @@ public class Shop {
         // Q: what should be the return type of this method?
     }
 
+    //Init the list of employees (salesmen)
     private void initEmployees() {
         Salesman salesman1 = new Salesman("Seder", "Nicu", "nicu123","password1");
         Salesman salesman2 = new Salesman("Popescu", "Victor","victor123", "password2");
@@ -125,6 +150,7 @@ public class Shop {
         Collections.addAll(shopEmployees, salesman1, salesman2, salesman3);
     }
 
+    //Method to initiate the list of cars in the shop
     private void initCars(){
         cars.add(new Logan(new Engine(1.0f,FuelType.GASOLINE),new BigDecimal(123),"Blue", new GregorianCalendar(2018,12,30),true));
         cars.add(new Focus(new Engine(2.0f,FuelType.ALTERNATIVE),new BigDecimal(178),"Red",new GregorianCalendar(2000,9,18),false));
@@ -143,19 +169,23 @@ public class Shop {
         cars.add(new Captur(new Engine(2.0f,FuelType.ALTERNATIVE),new BigDecimal(259),"Gold", new GregorianCalendar(2013,10,5),false));
     }
 
+    //Check salesman credentials
     private boolean checkCredentials(String username, String password, Salesman salesman) {
         if(salesman.getUsername().equals(username) && salesman.getPassword().equals(password)){
             return true;
         }
         return false;
     }
-    public void loginPage(){
+
+    //Just for design. Login welcoming message
+    private void loginPage(){
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------");
         System.out.println("|                                                        Jademy Car Rental Service                                                                  |");
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------");
         System.out.println("|                                                               Please Log In                                                                       |");
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------");
     }
+
     private String askUserName(){
         System.out.println("USER NAME:");
         return scanner.next();
@@ -164,23 +194,23 @@ public class Shop {
         System.out.println("PASSWORD:");
         return scanner.next();
     }
+    private String askMake(){
+        System.out.println("MAKE: ");
+        return scanner.next();
+    }
     private void successfulLogInMessage(){
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------");
         System.out.println("                                                           Log in successful!                                                                        ");
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------");
     }
-    private void listAllCars(){
-        showHeader();
-        for(Car car:cars){
-            car.showCarDetails();
-        }
-        showFooter();
-        carsOptionChecking();
-    }
+
+    //Just design for options
     private void allCarsOptions(){
         System.out.println("1. Actions                                                           2. Back                                                                  3. Exit");
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------");
     }
+
+    //Everytime we select an option an get a result we want to have options to go back, exit or do something else
     private void carsOptionChecking(){
         allCarsOptions();
         switch (scanner.nextInt()){
@@ -189,6 +219,16 @@ public class Shop {
             case 3:System.exit(0);
             default:break;
         }
+    }
+
+    //Listing al, available and rented cars
+    private void listAllCars(){
+        showHeader();
+        for(Car car:cars){
+            car.showCarDetails();
+        }
+        showFooter();
+        carsOptionChecking();
     }
     private void listAvailableCars(){
         showHeader();
@@ -210,6 +250,88 @@ public class Shop {
         showFooter();
         carsOptionChecking();
     }
+
+    //Checking income for cars which are marked as rented
+    private void checkIncome(){
+        int amount = 0;
+        for (Car car:cars){
+            if(!car.isAvailable()) {
+                amount += car.getBasePrice().intValue();
+            }
+        }
+        System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("Income of rented cars: $" + amount);
+        System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------");
+        carsOptionChecking();
+    }
+
+    //FILTERRING METHODS
+    private void filterByMake(){
+        System.out.println("KEYWORD FOR MAKE:");
+        List<Car> filteredByMakeCars = new ArrayList<>();
+        String result = scanner.next().toLowerCase().trim();
+        for(Car car:cars){
+            if(car.getMake().toLowerCase().trim().contains(result)){
+                filteredByMakeCars.add(car);
+            }
+        }
+        showHeader();
+        for(Car car:filteredByMakeCars){
+                car.showCarDetails();
+        }
+        showFooter();
+        carsOptionChecking();
+    }
+
+    private void filterByModel(){
+        System.out.println("KEYWORD FOR MODEL:");
+        List<Car> filteredByModelCars = new ArrayList<>();
+        String result = scanner.next().toLowerCase().trim();
+        for(Car car:cars){
+            if(car.getModel().toLowerCase().trim().contains(result)){
+                filteredByModelCars.add(car);
+            }
+        }
+        showHeader();
+        if(filteredByModelCars.size() != 0) {
+            for (Car car : filteredByModelCars) {
+                car.showCarDetails();
+            }
+        }else{
+            System.out.println("Sorry! No cars found with this keyword");
+            filterByModel();
+        }
+        showFooter();
+        carsOptionChecking();
+    }
+
+    private void filterByBudget(){
+        List<Car> filteredByBudgetCars = new ArrayList<>();
+        System.out.println("BOTTOM LIMIT:");
+        int bottom = scanner.nextInt();
+
+        System.out.println("TOP LIMIT:");
+        int top = scanner.nextInt();
+        for(Car car:cars){
+            if(car.getBasePrice().intValue() > bottom && car.getBasePrice().intValue() < top){
+                filteredByBudgetCars.add(car);
+            }
+        }
+        showHeader();
+        if(filteredByBudgetCars.size() != 0) {
+            for (Car car : filteredByBudgetCars) {
+                car.showCarDetails();
+            }
+        }else{
+            System.out.println("There are no cars in this price range");
+            filterByBudget();
+        }
+        showFooter();
+        System.out.println(top + bottom);
+        carsOptionChecking();
+    }
+
+    //HEADER & FOOTER (just for design)
     private void showHeader(){
         String header = String.format("%-15s%-15s%-19s%-15s%-15s%-15s%-15s%-15s%-15s%-15s","MAKE","MODEL","ENGINE","PRICE","DOORS","COLOR","TYPE","TRANSMISION","YEAR","AVAILABLE");
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------");
